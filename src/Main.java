096e79781f8094710adc66917ee8a95342b09d40
ArielFdz <88345754+ArielFdz@users.noreply.github.com>
2021-10-24 11:54:35 -0500

    ah

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
 public class Main{
-   public static void main(String[] args){
+   public static void main(String[] args) {
 
-   Empleado[] empleadosDePrueba=new Empleado[5];
-   Jornalero aux;
-   double nominaQuincenalTotal=0,nominaQuincenalTotalAdministrador=0,nominaQuincenalTotalJornalero=0;
+   Empleado[] empleadosDePrueba= new Empleado[5];
+   int i, numeroDeAdministradores,numeroDeJornaleros;
    double horasTotalEmpleados=0,horasTotalAdministrador=0,horasTotalJornalero=0; 
-   int i, numeroDeAdministradores=0,numeroDeJornaleros=0;
+   double totalNomina,totalNominaAdministrador,totalNominaJornalero;
+
+   //se inicializan las variables
+   numeroDeAdministradores=0;
+   numeroDeJornaleros=0;
+   totalNomina=0;
+   totalNominaAdministrador=0;
+   totalNominaJornalero=0;
+   
+   //datos de los empleados que usaremos para probar el funcionamiento del programa 
    empleadosDePrueba[0]=new Administrador("Wilberth Gamboa", 120);
    empleadosDePrueba[1]=new Jornalero("Luis Donaldo Colosio", 300);
    empleadosDePrueba[2]=new Administrador("Jacob Jesús", 200);
    empleadosDePrueba[3]=new Jornalero("Edgar Cambranes ", 96);
    empleadosDePrueba[4]=new Administrador("Jorge Carlos", 40);
 
+
    for(i=0;i<5;i++){
       if(empleadosDePrueba[i] instanceof Administrador){
          horasTotalAdministrador=horasTotalAdministrador+empleadosDePrueba[i].getNumHorasTrabajadas();
-         numeroDeAdministradores++;
          //PIDE LA TARIFA POR CADA EMPLEADO 
          empleadosDePrueba[i].setCalcularSalario(10);
          //REALIZA LA SUMATORAI DE LAS TARIFAS DE CADA EMPLEADO
-         nominaQuincenalTotalAdministrador= nominaQuincenalTotalAdministrador + empleadosDePrueba[i].getSalario();
+         totalNominaAdministrador= totalNominaAdministrador + empleadosDePrueba[i].getSalario();
+         numeroDeAdministradores=numeroDeAdministradores+1;
       }  
    }
 
 
-
+   Jornalero aux;
    for(i=0;i<5;i++){
       if(empleadosDePrueba[i] instanceof Jornalero){
          
@@ -37,12 +46,12 @@ public class Main{
          //Insertar la tarifa
          empleadosDePrueba[i].setCalcularSalario(10);
          //Total nomina quuincenal
-         nominaQuincenalTotalJornalero= nominaQuincenalTotalJornalero + empleadosDePrueba[i].getSalario();
-         numeroDeJornaleros++;
+         totalNominaJornalero= totalNominaJornalero + empleadosDePrueba[i].getSalario();
+         numeroDeJornaleros=numeroDeJornaleros+1;
       }   
    }
 
-   nominaQuincenalTotal=nominaQuincenalTotalAdministrador+nominaQuincenalTotalJornalero;
+   totalNomina=totalNominaAdministrador+totalNominaJornalero;
    horasTotalEmpleados=horasTotalAdministrador+horasTotalJornalero;
 
 
@@ -61,9 +70,9 @@ public class Main{
 
 
    //Total nomina quincenal
-   System.out.println("CANTIDAD DE NOMINA QUINCENAL TOTAL: "+nominaQuincenalTotal);
-   System.out.println("Nomina total quincenal trabajadores "+nominaQuincenalTotalAdministrador);
-   System.out.println("Nomina total quincenal jornalero "+nominaQuincenalTotalJornalero);
+   System.out.println("CANTIDAD DE NOMINA QUINCENAL TOTAL: "+totalNomina);
+   System.out.println("Nomina total quincenal trabajadores "+totalNominaAdministrador);
+   System.out.println("Nomina total quincenal jornalero "+totalNominaJornalero);
 
    //NUMERO DE TRABAJADORES
 
